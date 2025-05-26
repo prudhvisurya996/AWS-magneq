bc923691498df0e89c9636f9e6c5a7674639c7aa
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-05-26 21:20:38 +0530

    Update server-1.sh

--- server-1.sh
+++ server-1.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-sudo -i
-apt update -y
-sleep 2
-apt install apache2 -y
-git clone https://github.com/prudhvisurya996/AWS-magneq.git
-mkdir -p /var/www/html
-chown -R ubuntu:ubuntu /var/www/html
-cp -r Webpage-1/* /var/www/html
 
+# Update packages
+sudo apt update -y
+sleep 2
+
+# Install Apache
+sudo apt install apache2 -y
+
+# Clone the repo
+git clone https://github.com/prudhvisurya996/AWS-magneq.git
+
+# Ensure the target directory exists
+sudo mkdir -p /var/www/html
+
+# Change ownership to ubuntu user
+sudo chown -R ubuntu:ubuntu /var/www/html
+
+# Copy contents from cloned repo to web root
+cp -r AWS-magneq/Webpage-1/* /var/www/html
