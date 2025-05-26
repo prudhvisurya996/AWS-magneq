88c8620fa1ffcb427674ed9883d46541ae387c74
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-05-26 21:19:02 +0530

    Create server-1.sh

---
+++ server-1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo -i
+apt update -y
+sleep 2
+apt install apache2 -y
+git clone https://github.com/prudhvisurya996/AWS-magneq.git
+mkdir -p /var/www/html
+chown -R ubuntu:ubuntu /var/www/html
+cp -r Webpage-1/* /var/www/html
+
